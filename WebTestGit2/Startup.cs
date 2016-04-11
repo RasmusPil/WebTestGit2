e89b4b03fa8bb1b5ef5441c5d27042c85b00fe9b
Kim Rasmus Pil Rasmussen <rasmus.pil@live.dk>
2016-04-11 16:48:58 +0200

    Add project files.

---
+++ WebTestGit2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebTestGit2.Startup))]
+namespace WebTestGit2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
